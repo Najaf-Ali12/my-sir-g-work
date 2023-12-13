dfd33eb4f33b260e9ec52222dacaa9deca35822b
Najaf-Ali12 <150806918+Najaf-Ali12@users.noreply.github.com>
2023-12-13 07:53:55 -0800

    Add files via upload

---
+++ loop based exercises.py
@@ -0,0 +1,18 @@
+Python 3.4.0 (v3.4.0:04f714765c13, Mar 16 2014, 19:24:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> e=1
+while e<20:
+    print(e,end=" ")
+    e+=2
+    #or we can write it as
+g=1
+while e<=10:
+    print(2*e-1,end=" ")
+    e+=1
+c=1
+for i in range(16):
+    print(2*c-1,end=" ")
+    c+=1
+    
+
+      
